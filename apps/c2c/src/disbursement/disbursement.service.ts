67305cae7f2f192e908c79b494a4b148bac10848
subash-rumsan <subhash.parajuli@rumsan.net>
2025-10-31 14:01:50 +0545

    fix: calculate stash on disbursement completed

--- apps/c2c/src/disbursement/disbursement.service.ts
+++ apps/c2c/src/disbursement/disbursement.service.ts
@@ -446,6 +446,7 @@ export class DisbursementService {
                   ?.GroupedBeneficiaries?.length
               : 0,
         });
+        this.eventEmitter.emit(EVENTS.DISBURSEMENT_CREATE, {});
       }
 
       return disbursement;
